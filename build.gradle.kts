9b5ad3eba7a36fea69208810f6ac6f26fd1b0d85
John Turkson <github@johnturkson.com>
2019-12-07 23:55:05 -0800

    Renamed Main to Website, added jar export to gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,23 +12,41 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    compile("io.ktor:ktor-server-cio:1.2.6")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-cio:1.2.6")
+    implementation("org.slf4j:slf4j-nop:2.0.0-alpha1")
 }
 
 application {
-    mainClassName = "MainKt"
+    mainClassName = "WebsiteKt"
 }
 
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+    
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+    
     wrapper {
         gradleVersion = "6.0.1"
         distributionType = Wrapper.DistributionType.ALL
+    }
+    
+    jar {
+        from(sourceSets.main.get().output)
+        
+        dependsOn(configurations.runtimeClasspath)
+        
+        from({
+            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        })
+        
+        manifest {
+            attributes["Manifest-Version"] = "1.0"
+            attributes["Main-Class"] = "WebsiteKt"
+        }
     }
 }
--- src/main/kotlin/Website.kt
+++ src/main/kotlin/Website.kt
@@ -1,5 +1,4 @@
 import io.ktor.application.call
-import io.ktor.request.uri
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -8,10 +7,11 @@ import io.ktor.server.engine.embeddedServer
 
 fun main() {
     val website = embeddedServer(CIO, 80) {
+        println("Website running")
         routing {
             get("/") {
                 call.respondText { "Hello World" }
-                println("Received request from ${call.request.uri}")
+                println("Received request")
             }
         }
     }
