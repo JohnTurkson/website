43290441e78c48418a1f8044b6f5c0e0876e4df6
John Turkson <github@johnturkson.com>
2019-12-08 01:12:34 -0800

    Updated Routing

--- src/main/kotlin/Website.kt
+++ src/main/kotlin/Website.kt
@@ -1,20 +1,56 @@
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.HSTS
+import io.ktor.features.HttpsRedirect
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.cio.CIO
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 
 fun main() {
-    val website = embeddedServer(CIO, 80) {
-        println("Website running")
-        routing {
-            get("/") {
-                call.respondText { "Hello World" }
-                println("Received request")
-            }
+    val environment = applicationEngineEnvironment {
+        module {
+            main()
+        }
+        
+        connector {
+            host = "0.0.0.0"
+            port = 443
+        }
+        
+        connector {
+            host = "0.0.0.0"
+            port = 80
         }
     }
     
+    val website = embeddedServer(CIO, environment)
+    
     website.start(wait = true)
+}
+
+fun Application.main () {
+    // install(HSTS)
+    // install(HttpsRedirect) {
+    //     sslPort = 80
+    //     permanentRedirect = true
+    // }
+    
+    println("Website running")
+    
+    routing {
+        get("/") {
+            if (call.request.local.port == 80) {
+                println("Received HTTP request")
+            } else {
+                println("Received HTTPS request")
+            }
+            
+            call.respondText { "Hello World" }
+        }
+    }
 }
